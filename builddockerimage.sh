89afc2cb4ecf1b1262e0d0464b63c7f7239ed587
Alvaro.Barrera <70905730+alvarobfgit@users.noreply.github.com>
2026-01-22 15:57:59 +0100

    Create builddockerimage.sh

---
+++ builddockerimage.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# ALWAYS OPEN FROM THE TEXT EDITOR IN THE NAS. OTHERWISE PROBLEMS WITH /r IF EDITED IN WINDOWS
+# This script MUST be executed as ROOT user
+
+PATH="/volume1/sourcecode/GitHub/latest/fitbot-master"
+URL_DOCKER_FILE="/volume1/sourcecode/GitHub/latest/fitbot-master/Dockerfile"
+
+cd ${PATH}
+var=$(pwd)
+echo "The current working directory is: $var."
+
+/usr/local/bin/docker build -t fitbot-alvaro -f ${URL_DOCKER_FILE} .
+
+echo "Build finished"
