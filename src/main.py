af5eb826ebc381ca31d77831b459a84bcdd09a5c
Barrera Fernandez <alvaro.barrera@siemens-energy.com>
2026-05-29 10:18:34 +0200

    Adapted main file.

--- src/main.py
+++ src/main.py
@@ -5,12 +5,9 @@ from datetime import datetime, timedelta
 
 
 from client import AimHarderClient
-from exceptions import (
-    NoBookingGoal,
-    BoxClosed,
-    MESSAGE_BOX_IS_CLOSED,
-)
+from exceptions import NoBookingGoal, NoTrainingDay, BoxClosed, AlreadyBooked, TooEarly
 from exceptions import BookingFailed
+
 from logger import logger
 
 
@@ -27,17 +24,31 @@ def get_booking_goal_time(day: datetime, booking_goals):
         )
 
 
-def get_class_to_book(classes: list[dict], target_time: str, class_name: str):
-    if not classes or len(classes) == 0:
-        raise BoxClosed(MESSAGE_BOX_IS_CLOSED)
+def get_class_to_book(classes: list[dict], target_time: str, class_name: str) -> dict:
+    if len(classes) == 0:
+        logger.error(f"{user_name} - Box is closed.")
+        raise BoxClosed
 
-    classes = list(filter(lambda _class: target_time in _class["timeid"], classes))
-    _class = list(filter(lambda _class: class_name in _class["className"], classes))
-    if len(_class) == 0:
-        raise NoBookingGoal(
-            f"No class with the text `{class_name}` in its name at time `{target_time}`"
-        )
-    return _class[0]
+    if any(target_time in s["timeid"] for s in classes):
+        logger.info(f"{user_name} - Class found for time ({target_time})")
+        if "OPEN" in class_name:
+            found_classes = [s for s in classes if target_time in s["timeid"]]
+        else:
+            found_classes = [s for s in classes if target_time in s["timeid"] and 'OPEN' not in s['className']]
+    else:
+        logger.error(f"{user_name} - No class found for time ({target_time})")
+        raise NoBookingGoal(target_time)
+
+    if (len(found_classes)) > 1:
+        if any(class_name in s["className"] for s in found_classes):
+            logger.info(f"{user_name} - Class found for class name ({class_name})")
+            found_classes = [s for s in found_classes if class_name in s["className"]]
+        else:
+            logger.error(f"{user_name} - No class found for class name ({class_name})")
+            raise NoBookingGoal(class_name)
+
+    logger.info(f"{user_name} - Class found: {found_classes[0]}")
+    return found_classes[0]
 
 def wait_until_time(execution_time: str):
     """
